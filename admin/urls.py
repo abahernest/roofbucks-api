4160c812e35df7f2a9b7aa4d5de278e358ae2276
abahernest <abahernesto@gmail.com>
2023-08-21 05:43:53 +0100

    feat: fetch-property-ownership-requests EP

--- admin/urls.py
+++ admin/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import (KycVerificationApiView, PropertyModerationApiView, ReviewPropertyOwnershipRequest)
+from .views import (KycVerificationApiView, PropertyModerationApiView,
+                    ReviewPropertyOwnershipRequest, PropertyOwnershipRequestsViewset)
 
 urlpatterns = [
     path('kyc_verification/', KycVerificationApiView.as_view(), name='kyc_verification'),
     path('moderate_property/', PropertyModerationApiView.as_view(), name='moderate_property'),
     path('review_property_ownership/', ReviewPropertyOwnershipRequest.as_view(), name='review_property_ownership_request'),
+    path(
+        'property_ownership_requests/', PropertyOwnershipRequestsViewset.as_view({'get': 'list'}), name='property_ownership_requests'),
 ]
--- admin/views.py
+++ admin/views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from .serializers import (
     KycVerificationSerializer, PropertyModerationSerializer,
     ReviewPropertyOwnershipSerializer)
+from properties.serializers import  PropertyOwnershipSerializer, PropertyOwnership, \
+    CHOICES_FOR_PROPERTY_OWNERSHIP_STATUS, CHOICES_FOR_PROPERTY_OWNER_TYPE
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from utils.pagination import CustomPagination
+
 
 class KycVerificationApiView(views.APIView):
 
@@ -35,3 +40,24 @@ class ReviewPropertyOwnershipRequest(views.APIView):
         serializer.is_valid(raise_exception=True)
 
         return Response({"message": request.data['status']}, status=200)
+
+
+class PropertyOwnershipRequestsViewset(ReadOnlyModelViewSet):
+
+    serializer_class = PropertyOwnershipSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        queryset = PropertyOwnership.objects.filter()
+
+        status = self.request.query_params.get('status')
+        if status is not None:
+            queryset = queryset.filter(status__iexact=status)
+
+        user_type = self.request.query_params.get('user_type')
+        if user_type is not None:
+            queryset = queryset.filter(user_type__iexact=user_type)
+
+        queryset = queryset.order_by('-created_at')
+        return queryset
